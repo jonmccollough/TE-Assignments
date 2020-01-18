3f931bd7060d4cf33b812216ccd6075fb2739b15
Jon McCollough <jonathonmccollough@gmail.com>
2020-01-17 19:14:14 -0500

    LinearConvert

--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/DecimalToBinary.java
@@ -1,12 +1,19 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class DecimalToBinary {
 
 	public static void main(String[] args) {
 
+		Scanner myScanner  = new Scanner(System.in);
 		
+		System.out.println("Please enter in a series of decimal values (separated by spaces):");
 		
 		
 	}
 
 }
+
+
+// Integer.toBinaryString()
--- module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
+++ module-1/05_Command_Line_Programs/student-exercise/java/src/main/java/com/techelevator/LinearConvert.java
@@ -1,9 +1,44 @@
 package com.techelevator;
 
+import java.util.Scanner;
+
 public class LinearConvert {
 
 	public static void main(String[] args) {
 
+		Scanner myScanner = new Scanner(System.in);
+		
+		System.out.println("Please enter the length:");
+		
+		String strEnteredLength = myScanner.nextLine();
+		
+		System.out.println("Is the measurement in (m)eter, or (f)eet?");
+		
+		String strUnit = myScanner.nextLine();
+		
+		double enteredLength = Double.parseDouble(strEnteredLength);
+		
+		if(strUnit.equalsIgnoreCase("m")) {
+			
+			double meterToFoot = enteredLength * 3.2808399;
+			System.out.println(strEnteredLength + "m is " + meterToFoot + "f" );
+			
+		} else {
+			
+			double footToMeter = enteredLength * 0.3048;
+			System.out.println(strEnteredLength + "f is " + footToMeter + "m");
+		}
+		
 	}
 
 }
+
+
+//Write a program that converts meters to feet and vice-versa.
+//
+//The foot to meter conversion formula is:
+//
+//m = f * 0.3048
+//The meter to foot conversion formula is:
+//
+//f = m * 3.2808399
