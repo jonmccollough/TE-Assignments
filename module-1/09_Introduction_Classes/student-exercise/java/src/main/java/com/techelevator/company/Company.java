4f58c076d383e2e8cb807a8641a55442ea421cb0
Beth Campbell <beth@techelevator.com>
2020-01-23 12:56:27 -0500

    Adding 09 student-exercise

---
+++ module-1/09_Introduction_Classes/student-exercise/java/src/main/java/com/techelevator/company/Company.java
@@ -0,0 +1,17 @@
+package com.techelevator.company;
+
+public class Company {
+
+	private String name;
+	
+	
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
---
+++ module-1/09_Introduction_Classes/student-exercise/java/src/main/java/com/techelevator/dog/Dog.java
@@ -0,0 +1,11 @@
+package com.techelevator.dog;
+
+public class Dog {
+
+	private boolean isSleeping = false;
+	
+	public boolean isSleeping() {
+		return this.isSleeping;
+	}
+	
+}
