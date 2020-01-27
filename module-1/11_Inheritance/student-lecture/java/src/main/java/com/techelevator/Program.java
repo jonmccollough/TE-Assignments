d12caea5b2b8b16f456a39254b58c6d693b91dde
Jon McCollough <jonathonmccollough@gmail.com>
2020-01-27 13:48:48 -0500

    saving lesson

--- module-1/11_Inheritance/student-lecture/java/src/main/java/com/techelevator/Program.java
+++ module-1/11_Inheritance/student-lecture/java/src/main/java/com/techelevator/Program.java
@@ -5,33 +5,53 @@ import java.util.Scanner;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		Scanner userInput = new Scanner(System.in);
-		
+
 		boolean keepAskingForBids = true;
-		
+
 		Auction myAuction = new Auction("Garbage Can");
-		
+
 		System.out.println(myAuction.getItemForSale());
+
+		int bidCounter = 0;
+		while (bidCounter < 5) {
+			System.out.print("Please enter your name: ");
+			String bidderName = userInput.nextLine();
+
+			System.out.print("Please enter your bid amount ");
+			String strBidAmount = userInput.nextLine();
+			int bidAmount = Integer.parseInt(strBidAmount);
+
+			Bid nextBid = new Bid(bidderName, bidAmount);
+
+			myAuction.placeBid(nextBid);
+
+			System.out.println(nextBid);
+
+			myAuction.getHighBid();
+			
+			System.out.println(nextBid);
+			
+			System.out.println( "High Bid " + myAuction.getHighBid() );
+			
+			System.out.println( "All Bids " + myAuction.getAllBids() );
+
+			bidCounter++;
+			
+		}
 		
-		Bid myFinalOffer = new Bid("Beth", 5);
-		
-		System.out.print("Please enter your name: ");
-		String bidderName = userInput.nextLine();
-		
-		
-		System.out.print("Please enter your bid amount ");
-		String strBidAmount = userInput.nextLine();
-		int bidAmount = Integer.parseInt(strBidAmount);
-		
-		Bid nextBid = new Bid (bidderName, bidAmount);
-		
-		myAuction.placeBid(nextBid);
-		
-		System.out.println(nextBid);
-		
-		myAuction.getHighBid();
-	
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
