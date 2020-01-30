c8968ee6b953b6a00ae927b5214c5a67a8e7826a
Beth Campbell <beth@techelevator.com>
2020-01-30 10:39:13 -0500

    Added 14 student-lecture and corrected POM files for 12/13

---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/java/src/main/java/com/techelevator/farm/Cat.java
@@ -0,0 +1,23 @@
+package com.techelevator.farm;
+
+public final class Cat extends FarmAnimal {
+	
+	private boolean letOwnerSleep = false;
+
+	public Cat(String name, String sound) {
+		super(name, sound);
+	
+	}
+
+	@Override
+	public String eat(int howMuchFood) {
+		if (howMuchFood > 500) {
+			letOwnerSleep = true;
+		}
+		return "thanks, I'll wake you up at 4:30 AM for more food.";
+
+	}
+	
+	
+
+}
