c68d3b309f77e3ede12f0474a0fbc990cc7d013f
Jon McCollough <jonathonmccollough@gmail.com>
2020-01-31 09:18:48 -0500

    14 unit testing complete

--- module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/WordCountTest.java
+++ module-1/14_Unit_Testing/student-exercise/java/src/test/java/com/techelevator/WordCountTest.java
@@ -12,9 +12,41 @@ public class WordCountTest {
 	public void wordcount_black_sheep() {
 		WordCount myTest = new WordCount();
 		String[] words = {"ba", "ba", "black", "sheep"};
-		String[] ans = {"ba= 2", "black= 1", "sheep=1"};
+	//	String[] ans = {"ba= 2", "black= 1", "sheep=1"};
+		Map<String,Integer> ans  = new HashMap<String, Integer>() 
+		{{ 
+			put("ba" , 2); 
+			put("black", 1);
+			put("sheep", 1);
+		}};
 		Map<String, Integer> count = myTest.getCount(words);
 		
-		Assert.assertEquals(ans, count);
+		Assert.assertEquals( ans, count);
+	}
+	
+	@Test
+	public void wordcount_abc() {
+		WordCount myTest = new WordCount();
+		String[] words = {"a", "b", "a", "c", "b"};
+		Map<String,Integer> ans  = new HashMap<String, Integer>() 
+		{{ 
+			put("b" , 2); 
+			put("c", 1);
+			put("a", 2);
+		} };
+		Map<String, Integer> count = myTest.getCount(words);
+		
+		Assert.assertEquals( ans, count);
+	}
+	
+	@Test
+	public void wordcount_null() {
+		WordCount myTest = new WordCount();
+		Map<String,Integer> ans  = new HashMap<String, Integer>() 
+		{{ 
+		}};
+		Map<String, Integer> count = myTest.getCount(null);
+		
+		Assert.assertEquals( ans, count);
 	}
 }
