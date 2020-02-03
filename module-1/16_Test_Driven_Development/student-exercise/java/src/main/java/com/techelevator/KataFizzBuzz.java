cd6f840edcad5d1d88a134ad511d6dae1e22e760
Jon McCollough <jonathonmccollough@gmail.com>
2020-02-03 18:19:55 -0500

    fizz buzz initial 3 to fizz test

--- module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/main/java/com/techelevator/KataFizzBuzz.java
@@ -1,5 +1,14 @@
 package com.techelevator;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class KataFizzBuzz {
 
+	
+	public String fizzBuzz(int check) {
+		return null;
+
+	}
+	
 }
--- module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
+++ module-1/16_Test_Driven_Development/student-exercise/java/src/test/java/com/techelevator/KataFizzBuzzTest.java
@@ -7,4 +7,17 @@ import static org.junit.Assert.*;
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class KataFizzBuzzTest {
 
+	KataFizzBuzz tester = new KataFizzBuzz();
+	
+	
+	@Test
+	public void div_by_3_convert_to_fizz() {
+		
+		int check = 3;
+		
+		Assert.assertEquals( "fizz",  tester.fizzBuzz(check) );
+		
+	}
+
+	
 }
