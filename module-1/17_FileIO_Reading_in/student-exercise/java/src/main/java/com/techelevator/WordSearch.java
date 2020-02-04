62350154211ccd990a3f7a18461fe71caa112b2f
Jon McCollough <jonathonmccollough@gmail.com>
2020-02-04 18:59:50 -0500

    file io ignore case is finished-  exerciseis complete complete

--- module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
+++ module-1/17_FileIO_Reading_in/student-exercise/java/src/main/java/com/techelevator/WordSearch.java
@@ -2,8 +2,6 @@ package com.techelevator;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class WordSearch {
