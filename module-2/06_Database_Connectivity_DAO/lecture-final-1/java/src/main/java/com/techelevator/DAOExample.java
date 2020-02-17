476a9ad4a18ceb86901860a16eb24184f6053421
Walt <walt@techelevator.com>
2020-02-17 12:52:25 -0500

    Module 2, Day 6 - Lecture final

---
+++ module-2/06_Database_Connectivity_DAO/lecture-final-1/java/src/main/java/com/techelevator/DAOExample.java
@@ -0,0 +1,27 @@
+package com.techelevator;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import com.techelevator.city.City;
+import com.techelevator.city.CityDAO;
+import com.techelevator.city.JDBCCityDAO;
+
+public class DAOExample {
+
+	public static void main(String[] args) {
+	
+		CityFun funFunFun = new CityFun();
+		
+		City smallville = new City();
+		smallville.setCountryCode("USA");
+		smallville.setDistrict("KS");
+		smallville.setName("Smallville");
+		smallville.setPopulation(42080);
+		
+		funFunFun.saveCity(smallville);
+		
+		City theCity = funFunFun.findCityById(3);
+		
+	}
+
+}
