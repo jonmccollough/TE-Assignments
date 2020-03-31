4c0a52559d62210177807b2696a63787d9334214
Beth Campbell <beth@techelevator.com>
2020-03-31 14:25:42 -0400

    Adding exercise-final for module 3

---
+++ module-3/08-Session-and-Flash-Scope/exercise-final/dotnet/SessionExercise.Web/Controllers/FavoriteThingsController.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SessionExercise.Web.Extensions;
+using SessionExercise.Web.Models;
+
+namespace SessionExercise.Web.Controllers
+{
+    public class FavoriteThingsController : Controller
+    {
+        private const string FavoriteThingsKey = "FAVORITE_THINGS";
+
+        [HttpGet]
+        public IActionResult Page1()
+        {
+            HttpContext.Session.Set<FavoriteThings>(FavoriteThingsKey, new FavoriteThings());
+            return View(nameof(Page1));
+        }
+
+        [HttpPost]
+        public IActionResult Page1(string favoriteColor)
+        {
+            FavoriteThings favoriteThings = GetFavoriteThings();
+            favoriteThings.FavoriteColor = favoriteColor;
+            SaveFavoriteThings(favoriteThings);
+            return RedirectToAction(nameof(Page2));
+        }
+
+        [HttpGet]
+        public IActionResult Page2()
+        {
+            return View(nameof(Page2));
+        }
+
+        [HttpPost]
+        public IActionResult Page2(string favoriteFood)
+        {
+            FavoriteThings favoriteThings = GetFavoriteThings();
+            favoriteThings.FavoriteFood = favoriteFood;
+            SaveFavoriteThings(favoriteThings);
+            return RedirectToAction(nameof(Page3));
+        }
+
+        [HttpGet]
+        public IActionResult Page3()
+        {
+            return View(nameof(Page3));
+        }
+
+        [HttpPost]
+        public IActionResult Page3(string favoriteSeason)
+        {
+            FavoriteThings favoriteThings = GetFavoriteThings();
+            favoriteThings.FavoriteSeason = favoriteSeason;
+            SaveFavoriteThings(favoriteThings);
+            return RedirectToAction(nameof(Summary));
+        }
+
+        [HttpGet]
+        public IActionResult Summary()
+        {
+            FavoriteThings favoriteThings = GetFavoriteThings();
+            return View(nameof(Summary), favoriteThings);
+        }
+
+        private FavoriteThings GetFavoriteThings()
+        {
+            return HttpContext.Session.Get<FavoriteThings>(FavoriteThingsKey);
+        }
+
+        private void SaveFavoriteThings(FavoriteThings favoriteThings)
+        {
+            HttpContext.Session.Set<FavoriteThings>(FavoriteThingsKey, favoriteThings);
+        }
+    }
+}
